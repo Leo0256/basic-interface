631ef0e5f9e3689c04278881768d6da5d03fc316
Leonardo G. Ribeiro <leo.ribeiro0256@gmail.com>
2023-05-08 16:18:58 -0300

    mascara nos valores numéricos

--- src/App.js
+++ src/App.js
@@ -41,6 +41,33 @@ function App() {
 	const [ eventoSelect, setEvento ] = useState(null);
 	const [ maxParc, setMaxParc ] = useState(1);
 
+	const [ dinheiro, setDinheiro ] = useState('0,00')
+	const [ credito, setCredito ] = useState('0,00')
+	const [ debito, setDebito ] = useState('0,00')
+	const [ pix, setPix ] = useState('0,00')
+	const [ parcelaCredito, setParcelaCredito ] = useState('0,00')
+
+
+
+
+	function numberMask(value, percent) {
+		const percentMask = (text) => {
+			if(percent) {
+				if(text.includes('%'))
+					return text.replace('%', '').trim()
+				return text.trim().slice(0, -1)
+			}
+
+			return text.replace('R$', '').trim()
+		}
+
+		const number_aux = percentMask(value).replace('.', '').replace(',', '').replace(/\D/g, '');
+
+		return new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2 }).format(
+			parseFloat(number_aux) / 100
+		);
+	}
+
 	useEffect(() => {
 		axios.get(`${url}/getPdvs`)
 		.then(resp => {
@@ -167,129 +194,119 @@ function App() {
 					)) }
 				</datalist>
 			</label>
-			<label>
-				<span>Dinheiro: </span>
-				<input
-					type="number"
-					step="0.01"
-					min="0"
-					onChange={a => {
-						var value = parseFloat(!!a.target.value ? a.target.value : 0)
-						setTaxa(e => {
-							e.tax_dinheiro = !!e.tax_dinheiro_perc ? value / 100 : value
-							return e
-						})
-					}}
-				/>
-			</label>
-			<label>
-				<span className='auto'>Dinheiro em porcentagem: </span>
-				<input
-					type="checkbox"
-					onChange={() => setTaxa(e => {
-						e.tax_dinheiro_perc = !e.tax_dinheiro_perc
 
-						if(!!e.tax_dinheiro_perc)
-							e.tax_dinheiro /= 100
-						else
-							e.tax_dinheiro *= 100
+			<div className='input'>
+				<label>
+					<span>Dinheiro: </span>
+					<input
+						type="text"
+						value={taxa.tax_dinheiro_perc ? `${dinheiro} %` : `R$ ${dinheiro}`}
+						onChange={a => {
+							const value_mask = numberMask(a.target.value, taxa.tax_dinheiro_perc);
+							const value = parseFloat(value_mask.replace(/\./g,'').replace(/,/g,'.'));
 
-						return e
-					})}
-				/>
-			</label>
-			<label>
-				<span>Crédito: </span>
-				<input
-					type="number"
-					step="0.01"
-					min="0"
-					onChange={a => {
-						var value = parseFloat(!!a.target.value ? a.target.value : 0)
-						setTaxa(e => {
-							e.tax_credito = !!e.tax_credito_perc ? value / 100 : value
-							return e
-						})
-					}}
-				/>
-			</label>
-			<label>
-				<span className='auto'>Crédito em porcentagem: </span>
-				<input
-					type="checkbox"
-					onChange={() => setTaxa(e => {
-						e.tax_credito_perc = !e.tax_credito_perc
+							setDinheiro(value_mask);
+							setTaxa(a => ({ ...a, tax_dinheiro: taxa.tax_dinheiro_perc ? value / 100 : value }));
+						}}
+					/>
+				</label>
+				<label className='percent'>
+					<input
+						type="checkbox"
+						onChange={() => setTaxa(e => ({
+							...e,
+							tax_dinheiro: !e.tax_dinheiro_perc ? e.tax_dinheiro / 100 : e.tax_dinheiro * 100,
+							tax_dinheiro_perc: !e.tax_dinheiro_perc
+						}) )}
+					/>
+					<span>%</span>
+				</label>
+			</div>
 
-						if(!!e.tax_credito_perc)
-							e.tax_credito /= 100
-						else
-							e.tax_credito *= 100
+			<div className='input'>
+				<label>
+					<span>Crédito: </span>
+					<input
+						type="text"
+						value={taxa.tax_credito_perc ? `${credito} %` : `R$ ${credito}`}
+						onChange={a => {
+							const value_mask = numberMask(a.target.value, taxa.tax_credito_perc);
+							const value = parseFloat(value_mask.replace(/\./g,'').replace(/,/g,'.'));
 
-						return e
-					})}
-				/>
-			</label>
-			<label>
-				<span>Débito: </span>
-				<input
-					type="number"
-					step="0.01"
-					min="0"
-					onChange={a => {
-						var value = parseFloat(!!a.target.value ? a.target.value : 0)
-						setTaxa(e => {
-							e.tax_debito = !!e.tax_debito_perc ? value / 100 : value
-							return e
-						})
-					}}
-				/>
-			</label>
-			<label>
-				<span className='auto'>Débito em porcentagem: </span>
-				<input
-					type="checkbox"
-					onChange={() => setTaxa(e => {
-						e.tax_debito_perc = !e.tax_debito_perc
+							setCredito(value_mask);
+							setTaxa(a => ({ ...a, tax_credito: taxa.tax_credito_perc ? value / 100 : value }));
+						}}
+					/>
+				</label>
+				<label className='percent'>
+					<input
+						type="checkbox"
+						onChange={() => setTaxa(e => ({
+							...e,
+							tax_credito: !e.tax_credito_perc ? e.tax_credito / 100 : e.tax_credito * 100,
+							tax_credito_perc: !e.tax_credito_perc
+						}) )}
+					/>
+					<span>%</span>
+				</label>
+			</div>
 
-						if(!!e?.tax_debito_perc)
-							e.tax_debito /= 100
-						else
-							e.tax_debito *= 100
+			<div className='input'>
+				<label>
+					<span>Débito: </span>
+					<input
+						type="text"
+						value={taxa.tax_debito_perc ? `${debito} %` : `R$ ${debito}`}
+						onChange={a => {
+							const value_mask = numberMask(a.target.value, taxa.tax_debito_perc);
+							const value = parseFloat(value_mask.replace(/\./g,'').replace(/,/g,'.'));
 
-						return e
-					})}
-				/>
-			</label>
-			<label>
-				<span>Pix: </span>
-				<input
-					type="number"
-					step="0.01"
-					min="0"
-					onChange={a => {
-						var value = parseFloat(!!a.target.value ? a.target.value : 0)
-						setTaxa(e => {
-							e.tax_pix = !!e.tax_pix_perc ? value / 100 : value
-							return e
-						})}
-					}
-				/>
-			</label>
-			<label>
-				<span className='auto'>Pix em porcentagem: </span>
-				<input
-					type="checkbox"
-					onChange={() => setTaxa(e => {
-						e.tax_pix_perc = !e.tax_pix_perc
+							setDebito(value_mask);
+							setTaxa(a => ({ ...a, tax_debito: taxa.tax_debito_perc ? value / 100 : value }));
+						}}
+					/>
+				</label>
+				<label className='percent'>
+					<input
+						type="checkbox"
+						onChange={() => setTaxa(e => ({
+							...e,
+							tax_debito: !e.tax_debito_perc ? e.tax_debito / 100 : e.tax_debito * 100,
+							tax_debito_perc: !e.tax_debito_perc
+						}) )}
+					/>
+					<span>%</span>
+				</label>
+			</div>
 
-						if(!!e.tax_pix_perc)
-							e.tax_pix /= 100
-						else
-							e.tax_pix *= 100
-						return e
-					})}
-				/>
-			</label>
+			<div className='input'>
+				<label>
+					<span>Pix: </span>
+					<input
+						type="text"
+						value={taxa.tax_pix_perc ? `${pix} %` : `R$ ${pix}`}
+						onChange={a => {
+							const value_mask = numberMask(a.target.value, taxa.tax_pix_perc);
+							const value = parseFloat(value_mask.replace(/\./g,'').replace(/,/g,'.'));
+
+							setPix(value_mask);
+							setTaxa(a => ({ ...a, tax_pix: taxa.tax_pix_perc ? value / 100 : value }));
+						}}
+					/>
+				</label>
+				<label className='percent'>
+					<input
+						type="checkbox"
+						onChange={() => setTaxa(e => ({
+							...e,
+							tax_pix: !e.tax_pix_perc ? e.tax_pix / 100 : e.tax_pix * 100,
+							tax_pix_perc: !e.tax_pix_perc
+						}) )}
+					/>
+					<span>%</span>
+				</label>
+			</div>
+
 			<div>
 				<p>Parcelas de Crédito</p>
 
@@ -305,39 +322,38 @@ function App() {
 						})}
 					/>
 				</label>
-				<label>
-					<span>Taxa por parcela: </span>
-					<input
-						type="number"
-						step="0.01"
-						min="0"
-						onChange={a => {
-							var value = parseFloat(!!a.target.value ? a.target.value : 0)
-							setParc(e => {
-								e.par_acrescimo = !!e.par_acrescimo_perc ? value / 100 : value
-								return e
-							})
-					}}
-					/>
-				</label>
-				<label>
-					<span className='auto'>Parcelas em porcentagem: </span>
-					<input
-						type="checkbox"
-						onChange={() => setParc(e => {
-							e.par_acrescimo_perc = !e.par_acrescimo_perc
+				<div className='input'>
+					<label>
+						<span>Taxa por parcela: </span>
+						<input
+							type="text"
+							value={parc.par_acrescimo_perc ? `${parcelaCredito} %` : `R$ ${parcelaCredito}`}
+							onChange={a => {
+								const value_mask = numberMask(a.target.value, parc.par_acrescimo_perc);
+								const value = parseFloat(value_mask.replace(/\./g,'').replace(/,/g,'.'));
 
-							if(!!e.par_acrescimo_perc)
-								e.par_acrescimo /= 100
-							else
-								e.par_acrescimo *= 100
-							
-							return e
-						})}
-					/>
-				</label>
+								setParcelaCredito(value_mask);
+								setParc(e => ({
+									...e,
+									par_acrescimo: !!e.par_acrescimo_perc ? value / 100 : value
+								}));
+							}}
+						/>
+					</label>
+					<label className='percent'>
+						<input
+							type="checkbox"
+							onChange={() => setParc(e => ({
+								...e,
+								par_acrescimo: !e.par_acrescimo_perc ? e.par_acrescimo / 100 : e.par_acrescimo * 100,
+								par_acrescimo_perc: !e.par_acrescimo_perc
+							}) )}
+						/>
+						<span>%</span>
+					</label>
+				</div>
 			</div>
-			<button type="submit" disabled={!eventoSelect}>salvar taxa</button>
+			<button type="submit" disabled={!taxa.tax_classe}>salvar taxa</button>
 		</form>
 	</>
 }
