5d3376e5ae158fbd62f8d279315808c2aa475e5e
Leonardo G. Ribeiro <leo.ribeiro0256@gmail.com>
2023-07-18 09:53:40 -0300

    correção no endereço da API

--- src/model/index.js
+++ src/model/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default function Connection() {
-    //const url = 'https://temp-qingresso-taxas.onrender.com/temp';
-	const url = 'http://localhost:3000/temp';
+    const url = 'https://temp-qingresso-taxas.onrender.com/temp';
+	//const url = 'http://localhost:3000/temp';
 
     const conn = axios.create({
         baseURL: url
